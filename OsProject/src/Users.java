97a74915aacb1f1ac39ce202d1a4a05689a9c3bc
Sagheer <G00357770@gmit.ie>
2020-01-01 23:11:57 +0000

    Register/Login Started

---
+++ OsProject/src/Users.java
@@ -0,0 +1,28 @@
+
+public class Users {
+	
+	private String agentName;
+	private String Email;
+	private String agentId;
+	
+	public String getAgentName() {
+		return agentName;
+	}
+	public void setAgentName(String agentName) {
+		this.agentName = agentName;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public String getAgentId() {
+		return agentId;
+	}
+	public void setAgentId(String agentId) {
+		this.agentId = agentId;
+	}
+	
+
+}
