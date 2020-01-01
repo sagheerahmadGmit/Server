63326c93fb61c8c0a4e92e999bbd179773e67aa5
Sagheer <G00357770@gmit.ie>
2020-01-01 17:25:20 +0000

    first commit

---
+++ OsProject/src/Player.java
@@ -0,0 +1,60 @@
+
+public class Player {
+	
+	private String name;
+	private int age;
+	private int playerId;
+	private int clubId;
+	private int agentId;
+	private double value;
+	private String status;
+	private String position;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getPlayerId() {
+		return playerId;
+	}
+	public void setPlayerId(int playerId) {
+		this.playerId = playerId;
+	}
+	public int getClubId() {
+		return clubId;
+	}
+	public void setClubId(int clubId) {
+		this.clubId = clubId;
+	}
+	public int getAgentId() {
+		return agentId;
+	}
+	public void setAgentId(int agentId) {
+		this.agentId = agentId;
+	}
+	public double getValue() {
+		return value;
+	}
+	public void setValue(double value) {
+		this.value = value;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getPosition() {
+		return position;
+	}
+	public void setPosition(String position) {
+		this.position = position;
+	}
+}
